64933bfa39ea3b3652640dc021577d773aecc3ca
linducip2208 <lindu@act.co.id>
2026-07-06 11:57:42 +0700

    fix: exclude pos/checkout dari CSRF protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\RequirePair::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'pos/checkout',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
